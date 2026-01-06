2fd89cb977f0fddb71907d76da86f129d8bfd7c1
Takatsuka Chiaki <chiaki.takatsuka@epkotsoftware.co.jp>
2026-01-06 16:58:06 +0900

    ルーティング修正

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\ListController;
+use App\Http\Controllers\CalendarController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,14 +25,26 @@ Route::prefix('attendance')->name('attendance.')->group(function () {
         return view('attendance.index'); //機能一覧画面を表示
     })->name('index');
 
-    Route::get('/punch', function () {
-        return view('attendance.punch'); //勤怠打刻画面を表示
-    })->name('punch');
+    Route::get('/punch', [AttendanceController::class, 'index'])
+        ->name('punch');
 
     Route::post('/punch', [AttendanceController::class, 'store'])
         ->name('store');  //Attendance@store
 
-    Route::get('/list', [ListController::class, 'show'])
+    Route::get('/list', [ListController::class, 'index'])
         ->name('list');    //勤怠一覧画面を表示
 
+
+});
+Route::prefix('attendance')->name('attendance')->group(function () {
+    // attendance    attendance
+    Route::view('', 'attendance.index')->name('.index'); //     // \App\Http\Controllers\CalendarController
+    Route::prefix('calendars')->name('.calendars')->controller(CalendarController::class)->group(function () {
+        // attendance/calendar    attendance.calendar
+        Route::get('', 'index')->name('.index'); // attendance/calendars   attendance.calendar.index › CalendarController@index
+
+        Route::patch('{calendar}', 'update')->name('.update'); // attendance/calendar/{calendar}     attendance.calendar.update › CalendarController@update
+        Route::get('{calendar}/edit', 'edit')->name('.edit'); // attendance/calendar/{calendar}  /edit    admin.jobs.edit › CalendarController@edit
+
+    });
 });
