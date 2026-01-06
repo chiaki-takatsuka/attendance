c3afc814c67ad8fa9d43062fd1752add8c91ec71
Takatsuka Chiaki <chiaki.takatsuka@epkotsoftware.co.jp>
2026-01-06 17:24:56 +0900

    外部キーを修正するマイグレーションファイルを作成しました。

---
+++ database/migrations/2025_12_28_113327_add_attendance_users_fk_to_attendance.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('attendances', function (Blueprint $table) {
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('attendance_users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('attendances', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+    }
+};
