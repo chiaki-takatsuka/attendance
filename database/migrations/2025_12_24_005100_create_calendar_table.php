bc21e17f7f3de59f0e54b955a93e2f87dfb656a3
Takatsuka Chiaki <chiaki.takatsuka@epkotsoftware.co.jp>
2026-01-06 17:23:00 +0900

    calendarテーブルのマイグレーションファイルを作成しました。

---
+++ database/migrations/2025_12_24_005100_create_calendar_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calendars', function (Blueprint $table) {
+            $table->id()->comment('ID');
+            $table->date('calendar_date')->unique()->comment('日付');
+            $table->boolean('is_holiday')->comment('休日フラグ')->default(FALSE);
+            $table->string('holiday_name')->nullable()->comment('休日名');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calendars');
+    }
+};
